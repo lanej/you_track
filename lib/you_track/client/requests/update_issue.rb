8353a533ac65184307d6f7d235e9f1cf0601cc39
Josh Lane <me@joshualane.com>
2015-03-23 11:41:34 -0700

    restrict update issue attributes

--- lib/you_track/client/requests/update_issue.rb
+++ lib/you_track/client/requests/update_issue.rb
@@ -1,19 +1,33 @@
 # https://confluence.jetbrains.com/display/YTD6/Update+an+Issue
 class YouTrack::Client::UpdateIssue < YouTrack::Client::Request
-  def real(params={})
+  include YouTrack::Client::ParameterRequest
+
+  def self.accepted_attributes
+    # issueID # string  ID of an issue that should be updated.
+    @_accepted_attributes ||= [
+      "summary",     # string  New summary for the specified issue.
+      "description", # string  Updated description for the specified issue.
+    ]
+  end
+
+  def identity
+    params.fetch("id")
+  end
+
+  def accepted_attributes
+    Cistern::Hash.slice(params, *self.class.accepted_attributes)
+  end
+
+  def real
     service.request(
       :path   => "/issue/#{params.fetch("id")}",
       :method => :post,
-      :query  => params,
+      :query  => accepted_attributes,
     )
   end
 
-  def mock(params={})
-    id    = params.fetch("id")
-    issue = find(:issues, id)
-
-    issue.merge!(params)
-    service.data[:issues][id] = issue
+  def mock
+    find(:issues, identity).merge!(accepted_attributes)
 
     service.response
   end
